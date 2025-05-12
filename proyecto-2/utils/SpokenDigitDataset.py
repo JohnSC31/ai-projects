22b23f9f35ae6cc288501b014f3ee91ead7aa5b8
johnS31 <jostsace@estudiantec.cr>
2025-05-11 18:07:50 -0600

    augmentation frequency masking and time masking applied

--- proyecto-2/utils/SpokenDigitDataset.py
+++ proyecto-2/utils/SpokenDigitDataset.py
@@ -4,6 +4,7 @@ import librosa
 import torch
 import numpy as np
 import cv2
+import random
 
 class SpokenDigitDataset(Dataset):
 
@@ -47,10 +48,10 @@ class SpokenDigitDataset(Dataset):
 
         y, sr = librosa.load(filepath, sr=self.sr)
 
-          # 2. Augmentación opcional
-        if self.augment:
-            noise = np.random.normal(0, 0.005, size=y.shape)
-            y = y + noise
+        # se aplicar ruido
+        # if self.augment:
+            # noise = np.random.normal(0, 0.005, size=y.shape)
+            # y = y + noise
 
         # Extraer log-Mel espectrograma
         mel_spec = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=self.n_mels)
@@ -63,6 +64,9 @@ class SpokenDigitDataset(Dataset):
             log_mel_spec = cv2.bilateralFilter(log_mel_spec, d=9, sigmaColor=75, sigmaSpace=75)
             log_mel_spec = cv2.normalize(log_mel_spec, None, 0, 1, cv2.NORM_MINMAX)
 
+        if self.augment:
+            log_mel_spec = self.__apply_spec_augment(log_mel_spec)
+
         # 5. Redimensionar segun la configuracion
         resized_log_mel = cv2.resize(log_mel_spec, self.resize, interpolation=cv2.INTER_LINEAR)
 
@@ -70,3 +74,23 @@ class SpokenDigitDataset(Dataset):
         log_mel_tensor = torch.tensor(resized_log_mel, dtype=torch.float32).unsqueeze(0)
 
         return log_mel_tensor, label
+
+    def __apply_spec_augment(self, spec, freq_mask_param=20, time_mask_param=4, num_freq_masks=1, num_time_masks=1):
+
+        spec = spec.copy()
+
+        num_mels, num_frames = spec.shape
+
+        # Frequency masking
+        for _ in range(num_freq_masks):
+            f = random.randint(0, freq_mask_param)
+            f0 = random.randint(0, max(1, num_mels - f))
+            spec[f0:f0+f, :] = 0
+
+        # Time masking
+        for _ in range(num_time_masks):
+            t = random.randint(0, time_mask_param)
+            t0 = random.randint(0, max(1, num_frames - t))
+            spec[:, t0:t0+t] = 0
+
+        return spec
